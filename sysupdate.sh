2ef5b1722be6a13e60998f9db021052c869240f5
xcr0061 <xcr0061@gmail.com>
2023-08-26 16:11:44 +0400

    Complete script rebuild

--- sysupdate.sh
+++ sysupdate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-########## Pop-OS System Update #########
-#     APT - Flatpak - Pop Recovery      #
-#########################################
-#               by XCR0061
-
-set -o errexit
-#set -o xtrace
+##############################################
+#       APT - Flatpak - pop-OS Recovery      #
+#               SELECT SCRIPT                #
+#                                            #
+##############################################
+#                  by XCR0061                #
+##############################################
 
 clear
 tput setaf 208
@@ -18,53 +18,108 @@ echo "  #    /     \ \     \___|    |   \   # "
 echo "  #   /___/\  \ \______  /____|_  /   # "
 echo "  #         \_/        \/       \/    # "
 echo "  ##################################### "
-echo "              System Update             "
+echo "  #           System Update           # "
+echo "  ##################################### "
 echo ""
 tput sgr0
 
+##############################################
+############## Script Settings ###############
 
-# Heading - Sudo Password
-echo -e "$(tput setaf 6) $(tput bold) $(tput smul)\nAPT Update$(tput rmul) $(tput setaf 5) $(tput bold)\nPlease Enter Password... $(tput sgr0)"
-	echo
-        # APT system update
-		sudo apt update
-		echo
-		apt list --upgradable
-		echo
-		sudo apt dist-upgrade
-		echo
-        sleep 2
-        echo -e "$(tput setaf 6) $(tput bold) $(tput smul)\nAPT Auto Uninstall $(tput rmul) $(tput sgr0)"
-		sudo apt autoremove
-		echo
+## Exit If any command returns a non-zero
+set -o errexit
+##  Exit if an uninitialized variable is used
+set -o nounset
 
+## Uncomment For debugging
+#set -o xtrace
+
+##############################################
+# Display menu
 echo -e "$(tput setaf 6)\n================================================== $(tput sgr0)"
-
-
-# Heading - Flatpak
-echo -e "$(tput setaf 6) $(tput bold) $(tput smul)\nFlatpak Update$(tput rmul) $(tput setaf 5) $(tput bold)\nPlease Wait... $(tput sgr0)"
-	sleep 2
-	    echo
-	    # Flatpak system update
-		flatpak list --app --columns=name,application,runtime
-		echo
-        sleep 2
-		flatpak update
-	    echo
-        echo -e "$(tput setaf 6) $(tput bold) $(tput smul)\nFlatpak Uninstall Unused Apps & Runtime $(tput rmul) $(tput sgr0)"
-		sleep 2
-		flatpak uninstall --unused
-		echo
-
-echo -e "$(tput setaf 6)\n================================================== $(tput sgr0)"
-
-
-# Heading - Pop-OS Recovery update
-echo -e "$(tput setaf 6) $(tput bold) $(tput smul)\nPop-OS Recovery Update$(tput rmul) $(tput setaf 5) $(tput bold)\nPlease Wait... $(tput sgr0)"
-	sleep 2
-	echo
-        # Pop-OS Recovery update
-		pop-upgrade recovery upgrade from-release
-
-echo -e "$(tput setaf 6)\n================================================== $(tput sgr0)"
+echo -e "Please choose an option:"
 echo
+echo -e "1. Update everything"
+echo -e "2. Update APT only"
+echo -e "3. Update Flatpak only"
+echo -e "4. Update recovery only"
+echo -e "$(tput setaf 6)\n================================================== $(tput sgr0)"
+
+# Read the user choice
+read option
+
+##############################################
+# APT update function
+apt_update() {
+    echo -e "$(tput setaf 6) $(tput bold) $(tput smul)\nAPT Update$(tput rmul) $(tput setaf 5) $(tput bold)\nPlease Enter Password... $(tput sgr0)"
+    sudo apt update
+    echo
+    apt list --upgradable
+    echo
+    sleep 2
+    sudo apt dist-upgrade
+    echo
+    echo -e "$(tput setaf 6) $(tput bold) $(tput smul)\nAPT Auto Uninstall $(tput rmul) $(tput sgr0)"
+    sleep 2
+    sudo apt autoremove
+    echo
+}
+
+# Flatpak update function
+flatpak_update() {
+    echo
+    sleep 1
+    echo -e "$(tput setaf 6) $(tput bold) $(tput smul)\nFlatpak Update$(tput rmul) $(tput setaf 5) $(tput bold)\nPlease Wait... $(tput sgr0)"
+    echo
+    flatpak list --app --columns=name,application,runtime
+    echo
+    sleep 1
+    flatpak update
+    echo
+    echo -e "$(tput setaf 6) $(tput bold) $(tput smul)\nFlatpak Uninstall Unused Apps & Runtime $(tput rmul) $(tput sgr0)"
+    sleep 1
+    flatpak uninstall --unused
+    echo
+}
+
+# Pop-OS Recovery update function
+popos_recovery_update() {
+    sleep 1
+    echo -e "$(tput setaf 6) $(tput bold) $(tput smul)\nPop-OS Recovery Update$(tput rmul) $(tput setaf 5) $(tput bold)\nPlease Wait... $(tput sgr0)"
+    echo
+    pop-upgrade recovery upgrade from-release
+    echo
+}
+
+##############################################
+# Execute based on the chosen option
+case $option in
+    1)
+        echo -e "$(tput setaf 6)\n================================================== $(tput sgr0)"
+        echo
+        apt_update
+        echo
+        echo -e "$(tput setaf 6)\n================================================== $(tput sgr0)"
+        echo
+        flatpak_update
+        echo
+        echo -e "$(tput setaf 6)\n================================================== $(tput sgr0)"
+        echo
+        popos_recovery_update
+        echo
+        ;;
+    2)
+        apt_update
+        ;;
+    3)
+        flatpak_update
+        ;;
+    4)
+        popos_recovery_update
+        ;;
+    *)
+        echo -e "Invalid option"
+        ;;
+esac
+echo -e "$(tput setaf 6) $(tput bold)\nAll Done!! $(tput sgr0)"
+echo -e "$(tput setaf 6)\n================================================== $(tput sgr0)"
